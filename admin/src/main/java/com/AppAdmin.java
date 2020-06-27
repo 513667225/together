31c6db21e4d38747f5b96cf8d15525c316a216a0
taohua <513667225@qq.com>
2020-06-27 10:34:45 +0800

    删除无用信息

--- admin/src/main/java/com/AppAdmin.java
+++ admin/src/main/java/com/AppAdmin.java
@@ -3,12 +3,8 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 需要咨询java高级VIP课程的同学可以加白浅老师的QQ：2207192173
- * 需要视频资料的可以添加妮可老师的QQ：2860884084
- * author：鲁班学院-商鞅老师
- */
-//@EnableEurekaClient
+
+
 @SpringBootApplication
 public class AppAdmin {
 
