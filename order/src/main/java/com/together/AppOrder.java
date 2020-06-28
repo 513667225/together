f4a233c28a20080a190a43b01ad71a3d48538064
Administrator <2318595590@qq.com>
2020-06-28 21:58:17 +0800

    orderAPP

---
+++ order/src/main/java/com/together/AppOrder.java
@@ -0,0 +1,13 @@
+package com.together;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOrder {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppOrder.class);
+    }
+}
