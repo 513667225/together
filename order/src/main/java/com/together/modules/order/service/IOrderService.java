4a9a45728bdb07553ee6bc5e72d10799b6ee627a
Administrator <2318595590@qq.com>
2020-06-29 20:45:47 +0800

    service

--- order/src/main/java/com/together/modules/order/service/IOrderService.java
+++ order/src/main/java/com/together/modules/order/service/IOrderService.java
@@ -1,16 +1,27 @@
 package com.together.modules.order.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.together.modules.order.entity.OrderEntity;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.together.util.P;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * <p>
  * 订单表 服务类
  * </p>
  *
- * @author 
+ * @author liuhuan
  * @since 2020-06-28
  */
 public interface IOrderService extends IService<OrderEntity> {
 
+    /**
+     * 根据用户id查询订单信息+分页
+     */
+    IPage getOrderPage(@RequestParam("p")P p);
+
+    /**
+     * 增加订单方法
+     */
 }
