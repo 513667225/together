cb6474fd53b984f70276e641a01271efdc6d16b7
Administrator <2318595590@qq.com>
2020-06-29 20:46:01 +0800

    service

--- order/src/main/java/com/together/modules/order/service/impl/OrderServiceImpl.java
+++ order/src/main/java/com/together/modules/order/service/impl/OrderServiceImpl.java
@@ -1,9 +1,13 @@
 package com.together.modules.order.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.together.modules.order.entity.OrderEntity;
 import com.together.modules.order.mapper.OrderMapper;
 import com.together.modules.order.service.IOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.together.util.P;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +21,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderServiceImpl extends ServiceImpl<OrderMapper, OrderEntity> implements IOrderService {
 
+    @Override
+    public IPage getOrderPage(P p) {
+        Integer page = p.getInt("page");
+        Integer limit = p.getInt("limit");
+        Page page1 = new Page(page,limit);
+        Integer userId = (Integer)p.get("userId");
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("userId",userId);
+        return baseMapper.selectPage(page1,queryWrapper);
+    }
 }
