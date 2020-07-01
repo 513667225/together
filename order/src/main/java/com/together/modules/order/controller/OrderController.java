396e7550a60d7a8d5d7ebde9dc61150d7dca11dc
Administrator <2318595590@qq.com>
2020-07-01 19:40:22 +0800

    controller

--- order/src/main/java/com/together/modules/order/controller/OrderController.java
+++ order/src/main/java/com/together/modules/order/controller/OrderController.java
@@ -1,10 +1,15 @@
 package com.together.modules.order.controller;
 
 
-import com.baomidou.mybatisplus.extension.api.R;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.together.annotation.Pmap;
+import com.together.modules.order.entity.OrderEntity;
+import com.together.modules.order.mapper.OrderMapper;
 import com.together.modules.order.service.IOrderService;
+import com.together.modules.order.serviceClient.GoodsServiceClient;
 import com.together.util.P;
+import com.together.util.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,25 +24,45 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-06-28
  */
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/orders")
 public class OrderController {
 
     @Autowired
     private IOrderService iOrderService;
 
+
+    @Autowired
+    private GoodsServiceClient goodsServiceClient;
     /**
-     * 根据用户id 查询订单信息
+     * 查询订单信息
      */
 
+    @Autowired
+    OrderMapper orderMapper;
+
+    @RequestMapping("getOrderPage")
     public R getOrderPage(@Pmap P p){
-        return R.ok(iOrderService.getOrderPage(p));
+
+        Integer page = p.getInt("page");
+        Integer limit = p.getInt("limit");
+        p.initPageArg();
+        p.remove("page");
+        p.remove("limit");
+        p.remove("rowIndex");
+        Page<OrderEntity> objectPage = new Page<>(1, 10);
+        Page<OrderEntity> pageObject=orderMapper.selectPage(objectPage,new QueryWrapper<OrderEntity>().allEq(p));
+
+
+        return R.success("success",pageObject.getRecords()).set("count",pageObject.getTotal());
     }
+
+
 
     /**
      * 新增
      */
     public R insertOrderById(@Pmap P p){
-        return R.ok(iOrderService.insertOrderById(p));
+        return R.success("success",iOrderService.insertOrderById(p));
     }
 
     /**
