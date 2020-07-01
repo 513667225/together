02b59d08dd5776e74dc2360011de11daf59ef7a7
ly <1293876738@qq.com>
2020-07-01 15:25:58 +0800

    shop修改

--- gateway/src/main/java/com/together/serviceClient/ShopServiceClient.java
+++ gateway/src/main/java/com/together/serviceClient/ShopServiceClient.java
@@ -1,15 +1,15 @@
-package com.together.serviceClient;
-
-import com.together.util.R;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@FeignClient("SERVER-SHOP")
-@RequestMapping("/shop")
-public interface ShopServiceClient {
-
-    @RequestMapping("/getShopPage")
-    R getShopPage(int userId);
-
-
-}
+//package com.together.serviceClient;
+//
+//import com.together.util.R;
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@FeignClient("SERVER-SHOP")
+//@RequestMapping("/shop")
+//public interface ShopServiceClient {
+//
+//    @RequestMapping("/getShopPage")
+//    R getShopPage(int userId);
+//
+//
+//}
--- shop/src/main/java/com/together/modules/shop/service/IShopService.java
+++ shop/src/main/java/com/together/modules/shop/service/IShopService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.together.modules.shop.entity.ShopEntity;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.together.util.P;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -22,15 +23,15 @@ public interface IShopService extends IService<ShopEntity> {
      * 根据用户id查询店铺集合 分页
      * @return
      */
-    IPage getShopPage(@RequestParam("p") P p);
+    IPage getShopPage(P p);
 
     /**
      * 根据用户id新增店铺
      */
-    int saveShopById(@RequestParam("p") P p);
+    int saveShopById(P p);
 
     /**
      * 根据用户id修改店铺信息
      */
-    int updShopById(@RequestParam("p") P p);
+    int updShopById(P p);
 }
