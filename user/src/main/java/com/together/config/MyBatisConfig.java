f3eb2808cae6a93236fb86e57517b8d49c101833
coldest7 <coldest7@foxmail.com>
2020-07-02 10:28:42 +0800

    加了一个档次ID

--- user/src/main/java/com/together/config/MyBatisConfig.java
+++ user/src/main/java/com/together/config/MyBatisConfig.java
@@ -54,7 +54,7 @@ public class MyBatisConfig {
         dataSource.setDriverClassName(Driver.class.getName());
         dataSource.setPassword("root");
         dataSource.setUsername("root");
-        dataSource.setUrl("jdbc:mysql://192.168.0.109:3306/together");
+        dataSource.setUrl("jdbc:mysql://192.168.0.111/together");
         return dataSource;
     }
 
