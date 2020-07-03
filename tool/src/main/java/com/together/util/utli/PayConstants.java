18fcaa373940d8dda365c949115a8395318f0380
17722 <1772276596@qq.com>
2020-07-03 13:03:41 +0800

    user

---
+++ tool/src/main/java/com/together/util/utli/PayConstants.java
@@ -0,0 +1,68 @@
+package com.together.util.utli;
+
+/**
+ * 支付常量
+ */
+public class PayConstants {
+
+	/** 微信公众号 appid  */
+	public static final String WX_APPID = "wx97cc5113d8c1a92b";
+	
+	/** 微信公众号  secret  */
+	public static final String WX_SECRET = "40565b235c6ff677b53556a5517088d8";
+	
+	/** 微信商户号 partner    */
+	public static final String WX_PARTNER = "1600677836";
+	
+	/** 微信商户号的apiKey  partnerkey */
+	public static final String WX_PARTNERKEY = "aishangpin2020aishangpin20208888";
+	/** 微信统一支付接口  */
+	public static final String WX_PAY_URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+
+
+	//TODO: 微信支付 正式库
+	/** 会员注册微信支付后通知本地接口  */
+	public static final String WX_NOTIFY_LOCAL_URL = "https://xdgxc.com/weixinPay/PayResult";
+
+	/** 分享会员注册微信支付后通知本地接口  */
+	public static final String WX_NOTIFY_SHARE_URL = "https://xdgxc.com/getintegral/PayShareMember";
+
+	/** 用户下单微信支付后通知本地接口  */
+	public static final String WX_NOTIFY_USER_URL = "https://xdgxc.com/weixinPay/payOrderResult";
+
+	/** 用户房源订单微信支付后通知本地接口  */
+	public static final String WX_NOTIFY_PAYHOUSEORDERRESULT_URL = "https://xdgxc.com/houseOrder/payHouseOrderResult";
+
+	/** 拼单购物微信支付后通知本地接口  */
+	public static final String WX_NOTIFY_PAYGROUP_URL = "https://aishanggou.utools.club/user/getGroupUserState";
+
+	/** 用户扫码下单微信支付后通知本地接口  */
+	public static final String WX_NOTIFY_SCANPAYORDER_URL = "https://xdgxc.com/scan/scanPayOrderResult";
+
+	/** TODO: 用户充值余额微信支付后通知本地接口  */
+	public static final String WX_NOTIFY_RECHARGEBALANCE_URL = "https://xdgxc.com/rechargeOrder/rechargeBalanceResult";
+
+
+	//TODO: 微信支付 测试库
+	/** TODO: 会员注册微信支付后通知本地接口  */
+//	public static final String WX_NOTIFY_LOCAL_URL = "https://xdgxc.cn/weixinPay/PayResult";
+//
+//	/** TODO: 分享会员注册微信支付后通知本地接口  */
+//	public static final String WX_NOTIFY_SHARE_URL = "https://xdgxc.cn/getintegral/PayShareMember";
+//
+//	/** TODO: 用户下单微信支付后通知本地接口  */
+//	public static final String WX_NOTIFY_USER_URL = "https://xdgxc.cn/weixinPay/payOrderResult";
+//
+//	/** TODO: 用户房源订单微信支付后通知本地接口  */
+//	public static final String WX_NOTIFY_PAYHOUSEORDERRESULT_URL = "https://xdgxc.cn/houseOrder/payHouseOrderResult";
+//
+//	/** TODO: 拼单购物微信支付后通知本地接口  */
+//	public static final String WX_NOTIFY_PAYGROUP_URL = "https://xdgxc.cn/groupUser/payGroupResult";
+//
+//	/** TODO: 用户扫码下单微信支付后通知本地接口  */
+//	public static final String WX_NOTIFY_SCANPAYORDER_URL = "https://xdgxc.cn/scan/scanPayOrderResult";
+//
+//	/** TODO: 用户充值余额微信支付后通知本地接口  */
+//	public static final String WX_NOTIFY_RECHARGEBALANCE_URL = "https://xdgxc.cn/rechargeOrder/rechargeBalanceResult";
+
+}
