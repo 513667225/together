c6e70356da5e7f15a0b59b22bf196198bbcb5653
17722 <1772276596@qq.com>
2020-07-03 12:35:00 +0800

    user

---
+++ tool/src/main/java/com/together/util/utli/ResponseUtli.java
@@ -0,0 +1,21 @@
+package com.together.util.utli;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseUtli {
+    public static Map<String,Object> StringToMap(String key,Object value){
+        Map<String, Object> stringObjectMap = new HashMap<>();
+        if(value instanceof Integer){
+            stringObjectMap.put(key,Integer.valueOf(String.valueOf(value)));
+        }else{
+            stringObjectMap.put(key,value);
+        }
+        return stringObjectMap;
+    }
+
+    public static Map<String,Object> NullToMap(){
+        Map<String, Object> stringObjectMap = new HashMap<>();
+        return stringObjectMap;
+    }
+}
