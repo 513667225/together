deeea03ddfff54e2639e45fd13b2f6d2d3d971e3
Administrator <2318595590@qq.com>
2020-07-05 21:03:27 +0800

    server

--- shop/src/main/java/com/together/modules/shopUser/entity/ShopUserEntity.java
+++ shop/src/main/java/com/together/modules/shopUser/entity/ShopUserEntity.java
@@ -1,5 +1,6 @@
 package com.together.modules.shopUser.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.util.Date;
 import java.io.Serializable;
@@ -17,6 +18,7 @@ public class ShopUserEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @TableId(value = "shopuser_id")
     private Integer shopuserId;
 
     /**
