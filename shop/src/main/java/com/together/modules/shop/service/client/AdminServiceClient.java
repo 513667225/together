23521d1475b75025cbbd70dd1d6debe6c9b3645e
ly <1293876738@qq.com>
2020-07-09 18:08:25 +0800

    admin

---
+++ shop/src/main/java/com/together/modules/shop/service/client/AdminServiceClient.java
@@ -0,0 +1,16 @@
+package com.together.modules.shop.service.client;
+
+
+import com.together.util.P;
+import com.together.util.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("SERVER-ADMIN")
+@RequestMapping("/admin")
+public interface AdminServiceClient {
+
+    @RequestMapping("/getAdminByRegId")
+    public R getAdminByRegId(P p);
+
+}
